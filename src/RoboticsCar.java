ceacf5e8560a95a170cdc1ba37845f0ed6778e7d
octavius464 <msg2zinat@hotmail.com>
2018-11-09 13:55:23 +0000

    P control added

--- src/RoboticsCar.java
+++ src/RoboticsCar.java
@@ -1,21 +1,77 @@
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.SensorMode;
 import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 
+//Motor functions: forward(), stop(), rotateTo(),rotate(), setSpeed()
 public class RoboticsCar {
 	public static void main(String args[]){
-		Motor.A.setSpeed(720);// 2 RPM
-		Motor.C.setSpeed(720);
-		Motor.A.forward();
-		Motor.C.forward();
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Motor.A.stop();
-		Motor.C.stop();
-		Motor.A.rotateTo( 360);
-		Motor.A.rotate(-720,true);
+		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S4);
+		SensorMode reflectedLightMode = colorSensor.getRedMode();
+		float[] reflectedLightSample = new float[reflectedLightMode.sampleSize()];
+		
+		float white =(float) 0.66;
+		float black = (float )0.08;
+		float midPointValue = (white-black)/2 + black;
+		
+		// naive line following, without P or PID control
+	    /*while(true){
+			reflectedLightMode.fetchSample(reflectedLightSample, 0);	
+			float measuredValue = reflectedLightSample[reflectedLightMode.sampleSize()-1];
+			System.out.println(measuredValue);
+		    
+			if (measuredValue < midPointValue){
+				Motor.A.setSpeed(100);   // 720 : 2 RPM
+				Motor.A.forward();
+				Motor.C.setSpeed(50);
+				Motor.C.forward();
+			} else{
+				Motor.A.setSpeed(50);
+				Motor.A.forward();
+				Motor.C.setSpeed(100);
+				Motor.C.forward();
+			}    
+	    }*/
+	    
+	    //Proportional Control
+	    /*float Kp = 300;
+	    while(true){
+			reflectedLightMode.fetchSample(reflectedLightSample, 0);	
+			float measuredValue = reflectedLightSample[reflectedLightMode.sampleSize()-1];
+		    
+			float correction = Kp *(midPointValue-measuredValue);
+			// Turn each motor according to the correction
+			Motor.A.setSpeed(100+correction); 
+			Motor.A.forward();
+			Motor.C.setSpeed(100-correction);
+			Motor.C.forward();
+		    // If this doesn't work, can try rotate,			
+	    }*/
+		
+		//PID Control
+	    float Kp = 150;
+	    float Ki = 10;
+	    float Kd = 0;
+	    float lastError = 0;
+	    float integral = 0;
+	    while(true){
+			reflectedLightMode.fetchSample(reflectedLightSample, 0);	
+			float measuredValue = reflectedLightSample[reflectedLightMode.sampleSize()-1];
+		    float error = midPointValue -measuredValue;
+		    integral +=error;
+		    float derivative = error - lastError;
+		    lastError = error;
+		    
+			float correction = Kp *error + Ki*integral + Kd*derivative;
+			// Turn each motor according to the correction
+			Motor.A.setSpeed(100+correction); 
+			Motor.A.forward();
+			Motor.C.setSpeed(100-correction);
+			Motor.C.forward();
+			
+		    // If this doesn't work, can try rotate,			
+	    }
+	    
 	}
 }
--- src/Sensor.java
+++ src/Sensor.java
@@ -1,20 +1,23 @@
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.SensorMode;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.port.SensorPort;
 
 public class Sensor {
 	public static void main(String args[]){
 		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S4);
-		while(true){			
-			System.out.println(colorSensor.getColorID());
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		SensorMode reflectedLightMode = colorSensor.getRedMode();
+		float[] reflectedLightSample = new float[reflectedLightMode.sampleSize()];
 		
-		}
+	    while(true){
+			reflectedLightMode.fetchSample(reflectedLightSample, 0);	
+		    System.out.println(reflectedLightSample[reflectedLightMode.sampleSize()-1]);
+		    try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}	
+	    }
 		
 		
 	}
