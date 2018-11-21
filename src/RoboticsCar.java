cfbdf449bd367ad2e78f69a0679140ed3a8a25b0
octavius464 <msg2zinat@hotmail.com>
2018-11-21 11:59:14 +0000

    cleaned code

--- src/RoboticsCar.java
+++ src/RoboticsCar.java
@@ -1,85 +1,73 @@
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.sensor.SensorMode;
+import lejos.robotics.SampleProvider;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3ColorSensor;
 
 //Motor functions: forward(), stop(), rotateTo(),rotate(), setSpeed()
 public class RoboticsCar {
 	public static void main(String args[]){
-		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S4);
+		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S4);  // Set up Color Sensor
 		SensorMode reflectedLightMode = colorSensor.getRedMode();
 		float[] reflectedLightSample = new float[reflectedLightMode.sampleSize()];
 		
-		float white =(float) 0.66;
-		float black = (float )0.08;
+		EV3UltrasonicSensor ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S2);  // Set up Ultrasonic Sensor
+		ultrasonicSensor.enable();
+		SampleProvider distanceMode = ultrasonicSensor.getDistanceMode();
+		float[] distanceSample = new float[distanceMode.sampleSize()];
+		
+		float white =(float)0.66;
+		float black = (float)0.08;
 		float midPointValue = (white-black)/2 + black;
+	    boolean hasObstacle = false;
+	    float minObstDis = 100 ;
 		
-		// naive line following, without P or PID control
-	    /*while(true){
-			reflectedLightMode.fetchSample(reflectedLightSample, 0);	
-			float measuredValue = reflectedLightSample[reflectedLightMode.sampleSize()-1];
-			System.out.println(measuredValue);
-		    
-			if (measuredValue < midPointValue){
-				Motor.A.setSpeed(100);   // 720 : 2 RPM
-				Motor.A.forward();
-				Motor.C.setSpeed(50);
-				Motor.C.forward();
-			} else{
-				Motor.A.setSpeed(50);
-				Motor.A.forward();
-				Motor.C.setSpeed(100);
-				Motor.C.forward();
-			}    
-	    }*/
-	    
-	    //Proportional Control
-	    /*float Kp = 300;
-	    while(true){
-			reflectedLightMode.fetchSample(reflectedLightSample, 0);	
-			float measuredValue = reflectedLightSample[reflectedLightMode.sampleSize()-1];
-		    
-			float correction = Kp *(midPointValue-measuredValue);
-			// Turn each motor according to the correction
-			Motor.A.setSpeed(100+correction); 
-			Motor.A.forward();
-			Motor.C.setSpeed(100-correction);
-			Motor.C.forward();
-		    // If this doesn't work, can try rotate,			
-	    }*/
-		
-		//PID Control
+		//PI Control
 	    float Kp = 360;
-	    float Ki = 0;
+	    float Ki = (float) 0.001;
 	    float Kd = 0;
 	    float lastError = 0;
 	    float integral = 0;
 	    while(true){
-			reflectedLightMode.fetchSample(reflectedLightSample, 0);	
-			float measuredValue = reflectedLightSample[reflectedLightMode.sampleSize()-1];
-		    float error = midPointValue -measuredValue;
-		    
-		    integral +=error;
-		    if(error > -0.02 &&error < 0.02){
-		    	
-		    	integral = 0;
-		    }
-		    float derivative = error - lastError;
-		    lastError = error;
-		    
-			float correction = Kp *error + Ki*integral + Kd*derivative;
-			// Turn each motor according to the correction
-			Motor.A.setSpeed(150+correction); 
-			Motor.A.forward();
-			Motor.C.setSpeed(150-correction);
-			Motor.C.forward();
+	    	if (!hasObstacle){               // Algorithm for line following
+				reflectedLightMode.fetchSample(reflectedLightSample, 0);	
+				float measuredValue = reflectedLightSample[reflectedLightMode.sampleSize()-1];
+			    float error = midPointValue - measuredValue;	    
+			    integral +=error;
+			    if(error > -0.02 &&error < 0.02){ // Wind down integral term 	    	
+			    	integral = 0;
+			    }
+			    float derivative = error - lastError;
+			    lastError = error;		    
+				float correction = Kp *error + Ki*integral + Kd*derivative;
+				
+				// Differential drive according to correction
+				Motor.A.setSpeed(150+correction); 
+				Motor.A.forward();
+				Motor.C.setSpeed(150-correction);
+				Motor.C.forward();
+	    	}
+	    	
+	    	distanceMode.fetchSample(distanceSample, 0);   // Obstacle detection
+	    	float obstacleDistance = distanceSample[distanceMode.sampleSize()-1];
+	    	if( obstacleDistance <= minObstDis){
+	    		hasObstacle = true;
+	    	}
+	    	
+	    	if(hasObstacle){             // Algorithm for obstacle avoidance
+	    		
+	    		
+	    		
+	    		
+	    		
+	    	}
+			
 			try{
 				Thread.sleep(15);
 			} catch (InterruptedException e){
 				e.printStackTrace();
-			}
-		    // If this doesn't work, can try rotate,			
+			}			
 	    }
 	    
 	}
